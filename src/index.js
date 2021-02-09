406d6200378fa64f6f0425325b9880b845189e34
sergiocardenasg <sergiocardenas@utexas.edu>
2021-02-09 16:23:19 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const taskList = document.getElementById("tasks");
-  const form = document.getElementsByTagName("form");
-  let entry = document.createElement("li");
-
+  const form = document.getElementById("create-task-form");
+  
   function toDo(event) {
+    let entry = document.createElement("li");
+    let textNode = document.createTextNode(document.getElementById("new-task-description").value + " ");
+    entry.appendChild(textNode);
     taskList.appendChild(entry);
     event.preventDefault();
+
+    event.target.reset();
+
+    let deleteTask = document.createElement("button");
+    deleteTask.textContent = "X";
+    entry.appendChild(deleteTask);
+
+    function deleteItem() {
+        document.getElementById("tasks").removeChild(entry);
+    }
+
+    deleteTask.addEventListener("click", deleteItem);
   }
   
   form.addEventListener('submit', toDo);
