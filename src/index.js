3aa52e7c40b7e1c31913c839725fd4461ebfe27d
sergiocardenasg <sergiocardenas@utexas.edu>
2021-02-09 14:43:09 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskList = document.getElementById("tasks");
+  const form = document.getElementsByTagName("form");
+  let entry = document.createElement("li");
+
+  function toDo(event) {
+    taskList.appendChild(entry);
+    event.preventDefault();
+  }
+  
+  form.addEventListener('submit', toDo);
 });
